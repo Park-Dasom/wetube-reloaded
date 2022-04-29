26d0f82a03f4c897c5803b48d78f4c8e2baa23aa
DATTO-KKH <60614767+Park-Dasom@users.noreply.github.com>
2022-04-30 07:54:55 +0900

    Modified init.js
    Typing mistake in init.js..

--- src/init.js
+++ src/init.js
@@ -8,7 +8,7 @@ import app from "./server.js";
 
 const PORT = process.env.PORT || 4000;
 /* HEROKU에서 서버에 연결할 때는 HEROKU에서 제공하는 포트를 사용해야함.
-그 외에 내 컴퓨터에서 사용할 때는 localhost의 포트를 사용하도록 설정함. */ㄴ
+그 외에 내 컴퓨터에서 사용할 때는 localhost의 포트를 사용하도록 설정함. */
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
